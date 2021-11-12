c41766663ea276313addf11b139799501fe767d2
Atsushi <niconyan713@gmail.com>
2021-11-13 00:44:25 +0900

    first commit

---
+++ learning-graphql/photo-share-api/index.ts
@@ -0,0 +1,33 @@
+import { ApolloServer } from 'apollo-server';
+
+const typeDefs = `
+  type Query {
+    totalPhotos: Int!
+  }
+
+  type Mutation {
+    postPhoto(name: String! description: String): Boolean!
+  }
+`;
+
+const photos: string[] = [];
+const resolvers = {
+  Query: {
+    totalPhotos: () => photos.length
+  },
+  Mutation: {
+    postPhoto(parent, args) {
+      photos.push(args);
+      return true;
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server
+  .listen()
+  .then(({ url }) => console.log(`GraphQL Service running on ${url}`));
